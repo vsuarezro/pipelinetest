e792c294f520fa68f593c68791148e2f988ff62d
Victor Suarez (visuarez) <visuarez@cisco.com>
2021-11-09 10:17:27 -0600

    updated tests

--- tests.py
+++ tests.py
@@ -6,12 +6,10 @@ import pytest
 def test_yamls():
   files = [x for x in os.listdir(".") if x.endswith(("yaml","yml",))]
   print(files)
-  assert False
   for file in files:
-    print(file)
     with open(file, 'r') as stream:
       try:
-        return yaml.load(stream)
+        return yaml.safe_load(stream)
       except yaml.YAMLError as exception:
         raise exception
         
